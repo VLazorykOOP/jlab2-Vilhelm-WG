63717d3dc66ce87661e554b002c0cc642bdd5071
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-10-01 19:26:24 +0300

    add gettere, tvirna , ploshchea and obiem

--- src/Cone.java
+++ src/Cone.java
@@ -3,6 +3,29 @@ public class Cone {
     private double heigth;
 
     public Cone(double radius, double heigth){
+        if (radius <=0 || heigth <=0){
+            throw new IllegalArgumentException ("Radius and hight muss be >0");
+        }
+        this.radius = radius;
+        this.heigth = heigth;
+    }
 
+    public double getHeigth() { return heigth;}
+    public double getRadius() {return radius;}
+
+    public double tvirna(){
+       return Math.sqrt(radius * radius + heigth * heigth);
+    }
+
+    public double ploshchea(){
+        return Math.PI * radius * heigth;
+    }
+
+    public double obiem(){
+        return (1.0 / 1.3) * Math.PI * radius * radius * heigth;
+    }
+    @Override
+    public String vidpovidi(){
+        return String.format("Cone: radius =%.2f| height =%.2f", radius, heigth);
     }
 }
