6656fd56bf83b858e48c70c15930e1aad783fafe
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-10-01 19:04:58 +0300

    add task 2 Cone and ConeTest

---
+++ src/ConeTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ConeTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Введення даних
+        System.out.print("Введіть радіус основи конуса: ");
+        double r = sc.nextDouble();
+
+        System.out.print("Введіть висоту конуса: ");
+        double h = sc.nextDouble();
+
+        // Створення конуса
+        Cone cone = new Cone(r, h);
+
+        // Вивід результатів
+        System.out.println("\n" + cone);
+        System.out.printf("Твірна = %.2f\n", cone.getSlantHeight());
+        System.out.printf("Площа бічної поверхні = %.2f\n", cone.getLateralSurfaceArea());
+        System.out.printf("Об’єм = %.2f\n", cone.getVolume());
+
+        sc.close();
+    }
+}
