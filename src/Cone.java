844115ea8257d5897819bb046e1f1ba5362cfa20
Vilhelm-WG <haisiuk.vilhelm@chnu.edu.ua>
2025-10-01 19:09:31 +0300

    add class and constructor in Cone

--- src/Cone.java
+++ src/Cone.java
@@ -1,37 +1,8 @@
 public class Cone {
-    private double radius;  // радіус основи
-    private double height;  // висота
+    private double radius;
+    private double heigth;
 
-    // Конструктор
-    public Cone(double radius, double height) {
-        if (radius <= 0 || height <= 0) {
-            throw new IllegalArgumentException("Радіус і висота повинні бути > 0");
-        }
-        this.radius = radius;
-        this.height = height;
-    }
+    public Cone(double radius, double heigth){
 
-    // Геттери
-    public double getRadius() { return radius; }
-    public double getHeight() { return height; }
-
-    // Твірна l = sqrt(r^2 + h^2)
-    public double getSlantHeight() {
-        return Math.sqrt(radius * radius + height * height);
-    }
-
-    // Площа бічної поверхні
-    public double getLateralSurfaceArea() {
-        return Math.PI * radius * getSlantHeight();
-    }
-
-    // Об’єм
-    public double getVolume() {
-        return (1.0 / 3.0) * Math.PI * radius * radius * height;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Cone: radius=%.2f, height=%.2f ", radius, height);
     }
 }
